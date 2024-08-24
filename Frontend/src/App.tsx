b3bca074d431e0ef7619a0d583bb8367c2f7814e
Filip Kraljevski <filipce001@gmail.com>
2024-08-24 13:36:44 +0200

    Added Login Page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { UserProvider } from './Context/useAuth';
 
 function App() {
 
   return (
+    <UserProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +22,7 @@ function App() {
         </a>
       </header>
     </div>
+    </UserProvider>
   );
 }
 
