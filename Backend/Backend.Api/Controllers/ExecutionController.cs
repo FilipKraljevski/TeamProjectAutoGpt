cff4e7e11ef854a11744ad702cbbe45e3daae113
MartinAdzhiev <adzhievmartin@gmail.com>
2024-08-28 11:32:56 +0200

    Implemented extension method for extracting username from the token of the http request

---
+++ Backend/Backend.Api/Controllers/ExecutionController.cs
@@ -0,0 +1,29 @@
+﻿using Backend.Api.Extensions;
+using Backend.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Api.Controllers
+{
+    [Route("api/execution")]
+    [ApiController]
+    public class ExecutionController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ExecutionController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> getUsernameFromToken()
+        {
+            var username = User.GetUsername();
+            return Ok(username);
+        }
+    }
+}
